242c49e2ff4dfe3f0b244e7dd38575b424f70715
ManuCiao <mn.sabatino@gmail.com>
2017-01-23 17:37:24 +0000

    Add Rendertemplate lines and points to the layers

--- 4.x/bower/dojo/src/app/main.js
+++ 4.x/bower/dojo/src/app/main.js
@@ -2,15 +2,36 @@ define([
   "esri/Map",
   "esri/layers/FeatureLayer",
   "esri/views/MapView",
+  "esri/renderers/SimpleRenderer",
+  "esri/symbols/SimpleMarkerSymbol",
+  "esri/symbols/SimpleLineSymbol",
   "esri/PopupTemplate",
   "esri/widgets/Search",
   "esri/widgets/Search/SearchViewModel",
   "dojo/domReady!"
-], function (Map, FeatureLayer, MapView, PopupTemplate, Search, SearchVM) {
+], function (Map, FeatureLayer, MapView, SimpleRenderer,
+        SimpleMarkerSymbol, SimpleLineSymbol, PopupTemplate, Search, SearchVM) {
 
+  var sismaRenderer = new SimpleRenderer({
+         symbol: new SimpleMarkerSymbol({
+           size: 10,
+           color: "#FF4000",
+           outline: { // autocasts as new SimpleLineSymbol()
+             color: [255, 64, 0, 0.4], // autocasts as new Color()
+             width: 7
+           }
+         })
+       });
+
+   var tectonicRenderer = new SimpleRenderer({
+          symbol: new SimpleLineSymbol({
+            width: 1,
+            color: [64, 255, 0]
+          })
+        });
 
   var popupTemplate = new PopupTemplate({
-    title:"<b>Volcano Name: {Name}</b>",
+    title:"<b>Volcano: {Name}</b>",
     content: [{
       type: "fields",
       fieldInfos: [{
@@ -29,8 +50,27 @@ define([
     }]
   });
 
+  var popupTemplateSisma = new PopupTemplate({
+    title: "Seismic Activity: {location}",
+    content: [{
+      type: "fields",
+      fieldInfos: [{
+        fieldName: "magnitude",
+        visible: true,
+        label: "Magnitude (M): "
+      }, {
+        fieldName: "eventdate",
+        visible: true,
+        label: "Occurred at: "
+      }]
+    }]
+  });
+
   var tectonicPlates = new FeatureLayer({
-    url: "http://edumaps.esricanada.com/ArcGIS/rest/services/MapServices/TectonicPlates/MapServer"
+    url: "http://edumaps.esricanada.com/ArcGIS/rest/services/MapServices/TectonicPlates/MapServer",
+    renderer: tectonicRenderer,
+    minScale: 0,
+    maxScale: 0
   });
 
   var volcanoes = new FeatureLayer({
@@ -40,7 +80,10 @@ define([
   });
 
   var earthquakes = new FeatureLayer({
-    url: "http://earthquake.usgs.gov/arcgis/rest/services/eq/event_30DaySignificant/MapServer/0"
+    url: "http://earthquake.usgs.gov/arcgis/rest/services/eq/event_30DaySignificant/MapServer/0",
+    outFields: ["*"],
+    popupTemplate: popupTemplateSisma,
+    renderer: sismaRenderer
   });
 
   var layer = [tectonicPlates, volcanoes, earthquakes];
