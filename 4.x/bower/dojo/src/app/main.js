b06517fa0105ec0c8d47df39a5e0a8525cdff83d
ManuCiao <mn.sabatino@gmail.com>
2017-01-24 22:45:03 +0000

    render webmap with legend

--- 4.x/bower/dojo/src/app/main.js
+++ 4.x/bower/dojo/src/app/main.js
@@ -1,161 +1,68 @@
 define([
-    "esri/Map",
+    "esri/WebMap",
     "esri/views/MapView",
-    "esri/layers/FeatureLayer",
-    "esri/renderers/SimpleRenderer",
-    "esri/symbols/SimpleMarkerSymbol",
-    "esri/symbols/SimpleLineSymbol",
-    "esri/PopupTemplate",
-    "esri/widgets/Search",
-    "esri/widgets/Search/SearchViewModel",
     "esri/widgets/Legend",
+    "dojo/dom-construct",
+    "dojo/on",
+    "dojo/dom",
     "dojo/domReady!"
-], function(Map, MapView, FeatureLayer, SimpleRenderer,
-    SimpleMarkerSymbol, SimpleLineSymbol, PopupTemplate, Search, SearchVM, Legend) {
+], function(WebMap, MapView, Legend, domConstruct, on, dom) {
 
-    var sismaRenderer = new SimpleRenderer({
-        symbol: new SimpleMarkerSymbol({
-            color: "#FF4000",
-            outline: { // autocasts as new SimpleLineSymbol()
-                color: [255, 64, 0, 0.4], // autocasts as new Color()
-                width: 8
-            },
-            // label: "Seismic Activities scaled by Magnitude",
-            // visualVariables: [{
-            //   type: "size",
-            //   field: "magnitude",
-            //   legendOptions: {
-            //       title: "Sesmic Activities by Magnitude"
-            //   },
-            //   stops: [
-            //     { value: 4.0, size: 5, label: "< 4.0M" },
-            //     { value: 6.0, size: 8, label: "< 6.0M" },
-            //     { value: 8.0, size: 11, label: "< 8.0M" }
-            //   ]
-            // }]
+    var map = new WebMap({
+        portalItem: {
+            id: "059372802b014806913c75871ed5879b"
+        }
+    });
+
+    map.load()
+        .then(function() {
+            // load the basemap to get its layers created
+            return map.basemap.load();
         })
-    });
-
-    var tectonicRenderer = new SimpleRenderer({
-        symbol: new SimpleLineSymbol({
-            width: 2,
-            color: [255, 0, 0],
-            style: "short-dot"
+        .then(function() {
+            // grab all the layers and load them
+            var allLayers = map.allLayers;
+            var promises = allLayers.map(function(layer) {
+                return layer.load();
+            });
+            return all(promises.toArray());
         })
-    });
-
-    var popupTemplate = new PopupTemplate({
-        title: "<b>Volcano: {Name}</b>",
-        content: [{
-            type: "fields",
-            fieldInfos: [{
-                fieldName: "STATUS",
-                visible: true,
-                label: "Historical Status: "
-            }, {
-                fieldName: "TYPE",
-                visible: true,
-                label: "Description: "
-            }, {
-                fieldName: "SimpleType",
-                visible: true,
-                label: "Volcano Class: "
-            }]
-        }]
-    });
-
-    var popupTemplateSisma = new PopupTemplate({
-        title: "Seismic Activity: {location}",
-        content: [{
-            type: "fields",
-            fieldInfos: [{
-                fieldName: "magnitude",
-                visible: true,
-                label: "Magnitude (M): "
-            }, {
-                fieldName: "eventdate",
-                visible: true,
-                label: "Occurred at: "
-            }]
-        }]
-    });
-
-    var tectonicPlates = new FeatureLayer({
-        url: "http://edumaps.esricanada.com/ArcGIS/rest/services/MapServices/TectonicPlates/MapServer",
-        renderer: tectonicRenderer
-    });
-
-    var volcanoes = new FeatureLayer({
-        url: "http://services.arcgis.com/BG6nSlhZSAWtExvp/arcgis/rest/services/World_Volcanoes/FeatureServer",
-        outFields: ["*"],
-        popupTemplate: popupTemplate
-    });
-
-    var earthquakes = new FeatureLayer({
-        url: "http://earthquake.usgs.gov/arcgis/rest/services/eq/event_30DaySignificant/MapServer/0",
-        outFields: ["*"],
-        popupTemplate: popupTemplateSisma,
-        renderer: sismaRenderer
-    });
-
-
-    var layer = [tectonicPlates, volcanoes, earthquakes];
-
-    var map = new Map({
-        basemap: "dark-gray",
-        layers: layer
-    });
-
-    map.addMany(layer);
-
-    layer.forEach(function(items) {
-        items.watch("loadStatus", function(status) {
-            console.log("'" + items.title + "'" + " " + status);
-            if (status === "failed") {
-                console.log(items.loadError);
-            }
+        .then(function(layers) {
+            // each layer load promise resolves with the layer
+            console.log("all " + layers.length + " layers loaded");
+        })
+        .otherwise(function(error) {
+            console.error(error);
         });
-    });
 
-    var view = new MapView({
-        container: "viewDiv",
-        map: map,
-        scale: 35000000,
-        center: [11.527454, 52.57867],
 
-        popup: {
-            dockEnabled: true,
-            dockOptions: {
-                buttonEnabled: true,
-                position: "bottom-right"
-            }
-        }
+var view = new MapView({
+    container: "viewDiv",
+    map: map,
+    scale: 35000000,
+    center: [11.527454, 52.57867]
+});
+view.ui.add(dom.byId("container"), "top-right");
 
-    });
+view.then(function() {
+var tectonicPlates = map.layers.getItemAt(0);
+var earthquakes = map.layers.getItemAt(4);
+var volcanoes = map.layers.getItemAt(3);
+var legend = new Legend({
+    view: view,
+    layerInfos: [{
+        layer: tectonicPlates,
+        title: "Tectonic Plates"
+    }, {
+        layer: volcanoes,
+        title: "Volcanoes"
+    }, {
+        layer: earthquakes,
+        title: "Seismic Activities (over 30 days)"
+    }]
+});
 
-    var searchWidget = new Search({
-        viewModel: new SearchVM({
-            view: view
-        })
-    }, "searchDiv");
+view.ui.add(legend, "bottom-right");
+});
 
-    var legend = new Legend({
-        view: view,
-        layerInfos: [
-          {
-            layer: earthquakes,
-            title: "Seismic Activities(over 30 days)"
-        }, {
-          layer: tectonicPlates,
-          title: "Tectonic Plates"
-        }, {
-          layer: volcanoes,
-          title: "Volcanoes"
-        }
-      ]
-    });
-
-    view.ui.add(legend, "bottom-right");
-
-    searchWidget.startup();
 });
